413ac152912a0b5a055b5649862bb42148bed418
alysangadji <alysangadji@hotmail.com>
2014-05-26 01:43:00 -0700

    add prject

---
+++ SampleSceneManager/app/scenes/Scene1.js
@@ -0,0 +1,60 @@
+alert('SceneScene1.js loaded');
+
+function SceneScene1() {
+
+};
+
+SceneScene1.prototype.initialize = function () {
+	alert("SceneScene1.initialize()");
+	// this function will be called only once when the scene manager show this scene first time
+	// initialize the scene controls and styles, and initialize your variables here
+	// scene HTML and CSS will be loaded before this function is called
+	$('#svecLabel_onoy').sfLabel({
+		text:'Scene One'
+	});
+};
+
+SceneScene1.prototype.handleShow = function (data) {
+	alert("SceneScene1.handleShow()");
+	// this function will be called when the scene manager show this scene
+};
+
+SceneScene1.prototype.handleHide = function () {
+	alert("SceneScene1.handleHide()");
+	// this function will be called when the scene manager hide this scene
+};
+
+SceneScene1.prototype.handleFocus = function () {
+	alert("SceneScene1.handleFocus()");
+	// this function will be called when the scene manager focus this scene
+};
+
+SceneScene1.prototype.handleBlur = function () {
+	alert("SceneScene1.handleBlur()");
+	// this function will be called when the scene manager move focus to another scene from this scene
+};
+
+SceneScene1.prototype.handleKeyDown = function (keyCode) {
+	alert("SceneScene1.handleKeyDown(" + keyCode + ")");
+	// TODO : write an key event handler when this scene get focused
+	switch (keyCode) {
+		case sf.key.LEFT:
+			break;
+		case sf.key.RIGHT:
+			sf.scene.hide("Scene1");
+			sf.scene.show("Scene2", "This is an example of samsung smart TV app with javascript programming language.");
+			sf.scene.focus("Scene2");
+			break;
+		case sf.key.UP:
+			break;
+		case sf.key.DOWN:
+			break;
+		case sf.key.ENTER:
+			break;
+		case sf.key.RETURN:
+			break;
+		default:
+			alert("handle default key event, key code(" + keyCode + ")");
+			break;
+	}
+};
